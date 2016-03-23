b7e2eb62d5427db81f27cf84b488a3ffa75d71cc
Camilo <camilonar09@gmail.com>
2016-03-23 15:25:28 -0500

    Se creó la clase contrato con la Base de Datos DataBaseContract

--- app/src/main/java/epiphany_soft/wtw/ActivityConsultarPelicula.java
+++ app/src/main/java/epiphany_soft/wtw/ActivityConsultarPelicula.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-
 /**
  * Created by Camilo on 22/03/2016.
  */

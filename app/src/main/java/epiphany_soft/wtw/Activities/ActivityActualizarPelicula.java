047dcf02e592430d4ba7e2f4396e23d93f17e225
Camilo <camilonar09@gmail.com>
2016-03-27 19:57:39 -0500

    Se agregó ActivityActualizarPelicula para actualizar películas, pero no tiene funcionalidad

---
+++ app/src/main/java/epiphany_soft/wtw/Activities/ActivityActualizarPelicula.java
@@ -0,0 +1,56 @@
+package epiphany_soft.wtw.Activities;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import epiphany_soft.wtw.DataBase.DataBaseConnection;
+import epiphany_soft.wtw.DataBase.DataBaseContract;
+import epiphany_soft.wtw.Negocio.Genero;
+import epiphany_soft.wtw.R;
+
+/**
+ * Created by Camilo on 27/03/2016.
+ */
+public class ActivityActualizarPelicula extends AppCompatActivity {
+    private EditText name,sinopsis;
+    private Spinner spnGenero;
+    String nombrePelicula;
+    String sinopsisText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_pelicula);
+        name=(EditText)findViewById(R.id.txtNombrePelicula);
+        name.setKeyListener(null);
+        sinopsis=(EditText)findViewById(R.id.txtSinopsis);
+        Bundle b = getIntent().getExtras();
+        nombrePelicula = b.getString(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_NOMBRE);
+        sinopsisText = b.getString(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_SINOPSIS);
+        sinopsis.setText(sinopsisText);
+        name.setText(nombrePelicula);
+        crearSpinnerGeneros();
+    }
+
+    public void crearSpinnerGeneros(){
+        spnGenero = (Spinner) findViewById(R.id.spnGeneroAct);
+        DataBaseConnection db=new DataBaseConnection(this.getBaseContext());
+        Cursor c=db.consultarAllGeneros();
+        ArrayList<Genero> generos = new ArrayList<Genero>();
+        while (c.moveToNext()){
+            Genero g=
+                    new Genero(c.getInt(c.getColumnIndex(DataBaseContract.GeneroContract.COLUMN_NAME_GENERO_ID)),
+                            c.getString(c.getColumnIndex(DataBaseContract.GeneroContract.COLUMN_NAME_GENERO_NOMBRE)));
+            generos.add(g);
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.simple_spinner_item,generos);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spnGenero.setAdapter(adapter);
+    }
+}
--- app/src/main/java/epiphany_soft/wtw/Activities/ActivityDetallePelicula.java
+++ app/src/main/java/epiphany_soft/wtw/Activities/ActivityDetallePelicula.java
@@ -1,8 +1,10 @@
 package epiphany_soft.wtw.Activities;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import epiphany_soft.wtw.DataBase.DataBaseConnection;
@@ -13,6 +15,9 @@ import epiphany_soft.wtw.R;
  * Created by Camilo on 26/03/2016.
  */
 public class ActivityDetallePelicula extends AppCompatActivity {
+    String nombre;
+    String sinopsis;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_pelicula);
@@ -27,11 +32,20 @@ public class ActivityDetallePelicula extends AppCompatActivity {
         DataBaseConnection db=new DataBaseConnection(this.getBaseContext());
         Cursor c=db.consultarPeliculaPorNombre(nombrePelicula);
         c.moveToNext();
-        String nombre=c.getString(c.getColumnIndex(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_NOMBRE));
-        String sinopsis=c.getString(c.getColumnIndex(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_SINOPSIS));
+        nombre=c.getString(c.getColumnIndex(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_NOMBRE));
+        sinopsis=c.getString(c.getColumnIndex(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_SINOPSIS));
         if (nombre!=null) ((TextView) findViewById(R.id.txtNombrePel)).setText(nombre);
         else ((TextView) findViewById(R.id.txtNombrePel)).setText("Película sin nombre");
         if (sinopsis!=null) ((TextView) findViewById(R.id.txtSinopsisPel)).setText(sinopsis);
         else ((TextView) findViewById(R.id.txtSinopsisPel)).setText("Película sin sinopsis");
+    }
+
+    public void onClickActualizarPelicula(View v){
+        Intent i = new Intent(this, ActivityActualizarPelicula.class);
+        Bundle b = new Bundle();
+        b.putString(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_NOMBRE,nombre);
+        b.putString(DataBaseContract.ProgramaContract.COLUMN_NAME_PROGRAMA_SINOPSIS,sinopsis);
+        i.putExtras(b);
+        startActivity(i);
     }
 }

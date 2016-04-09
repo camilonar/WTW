774afbf19e3b9e916277f7316e3e94b925b60463
Camilo <camilonar09@gmail.com>
2016-04-09 18:19:59 -0500

    Se agregó una actividad para gestionar todas las opciones del usuario

--- app/src/main/java/epiphany_soft/wtw/Activities/ActivityGestionUsuario.java
+++ app/src/main/java/epiphany_soft/wtw/Activities/ActivityGestionUsuario.java
@@ -1,9 +1,62 @@
 package epiphany_soft.wtw.Activities;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import epiphany_soft.wtw.ActivityBase;
+import epiphany_soft.wtw.Negocio.Sesion;
+import epiphany_soft.wtw.R;
 
 /**
  * Created by Camilo on 9/04/2016.
  */
 public class ActivityGestionUsuario extends ActivityBase {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_usuario);
+        setTitle("GESTION DE USUARIO");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (Sesion.getInstance().isActiva()) {
+            Button b = (Button) findViewById(R.id.btnInicioSesion);
+            b.setEnabled(false);
+            b.setTextColor(Color.GRAY);
+            b = (Button) findViewById(R.id.btnCerrarSesion);
+            b.setEnabled(true);
+            b.setTextColor(this.getResources().getColor(R.color.colorButtonText));
+            b= (Button) findViewById(R.id.btnModificarUsuario);
+            b.setEnabled(true);
+            b.setTextColor(this.getResources().getColor(R.color.colorButtonText));
+        }
+    }
+
+    public void onClickIniciarSesion(View v){
+        Intent i = new Intent(this, ActivityInicioSesion.class);
+        startActivity(i);
+    }
+
+    public void onClickCerrarSesion(View v){
+        Sesion.getInstance().refresh();
+        Button b = (Button) findViewById(R.id.btnInicioSesion);
+        b.setEnabled(true);
+        b.setTextColor(this.getResources().getColor(R.color.colorButtonText));
+        b = (Button) findViewById(R.id.btnCerrarSesion);
+        b.setEnabled(false);
+        b.setTextColor(Color.GRAY);
+        b = (Button) findViewById(R.id.btnModificarUsuario);
+        b.setEnabled(false);
+        b.setTextColor(Color.GRAY);
+    }
+
+    public void onClickModificarUsuario(View v){
+        Intent i = new Intent(this, ActivityModificarUsuario.class);
+        startActivity(i);
+    }
 }
--- app/src/main/java/epiphany_soft/wtw/Activities/MainActivity.java
+++ app/src/main/java/epiphany_soft/wtw/Activities/MainActivity.java
@@ -1,14 +1,11 @@
 package epiphany_soft.wtw.Activities;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import epiphany_soft.wtw.Activities.Series.ActivityConsultarSerie;
-import epiphany_soft.wtw.Negocio.Sesion;
 import epiphany_soft.wtw.R;
 
 public class MainActivity extends AppCompatActivity{
@@ -17,22 +14,6 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        if (Sesion.getInstance().isActiva()) {
-            Button b = (Button) findViewById(R.id.btnInicioSesion);
-            b.setEnabled(false);
-            b.setTextColor(Color.GRAY);
-            b = (Button) findViewById(R.id.btnCerrarSesion);
-            b.setEnabled(true);
-            b.setTextColor(this.getResources().getColor(R.color.colorButtonText));
-            b= (Button) findViewById(R.id.btnModificarUsuario);
-            b.setEnabled(true);
-            b.setTextColor(this.getResources().getColor(R.color.colorButtonText));
-        }
     }
 
     public void onClickConsultar(View v){
@@ -60,23 +41,8 @@ public class MainActivity extends AppCompatActivity{
         startActivity(i);
     }
 
-    public void onClickIniciarSesion(View v){
-        Intent i = new Intent(this, ActivityInicioSesion.class);
-        startActivity(i);
-    }
-
-    public void onClickCerrarSesion(View v){
-        Sesion.getInstance().refresh();
-        Button b = (Button) findViewById(R.id.btnInicioSesion);
-        b.setEnabled(true);
-        b.setTextColor(this.getResources().getColor(R.color.colorButtonText));
-        b = (Button) findViewById(R.id.btnCerrarSesion);
-        b.setEnabled(false);
-        b.setTextColor(Color.GRAY);
-    }
-
-    public void onClickModificarUsuario(View v){
-        Intent i = new Intent(this, ActivityModificarUsuario.class);
+    public void onClickGestionUsuario(View v){
+        Intent i = new Intent(this, ActivityGestionUsuario.class);
         startActivity(i);
     }
 }

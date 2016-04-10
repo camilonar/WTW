045ed1f0495a8636b6cb9207c55ce6582e0a94e9
Camilo <camilonar09@gmail.com>
2016-04-10 17:10:03 -0500

    Se deshabilitan ciertas opciones cuando el usuario no ha iniciado sesión

--- app/src/main/java/epiphany_soft/wtw/Activities/MainActivity.java
+++ app/src/main/java/epiphany_soft/wtw/Activities/MainActivity.java
@@ -1,11 +1,14 @@
 package epiphany_soft.wtw.Activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import epiphany_soft.wtw.Activities.Series.ActivityConsultarSerie;
+import epiphany_soft.wtw.Negocio.Sesion;
 import epiphany_soft.wtw.R;
 
 public class MainActivity extends AppCompatActivity{
@@ -14,6 +17,45 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        hideWhenNoSesion();
+        showWhenSesion();
+    }
+
+    public void hideWhenNoSesion(){
+        if (!Sesion.getInstance().isActiva()){
+            Button b = (Button) findViewById(R.id.btnGenero);
+            hide(b);
+            b= (Button) findViewById(R.id.btnEliminarGenero);
+            hide(b);
+            b = (Button) findViewById(R.id.btnAgregarPrograma);
+            hide(b);
+        }
+    }
+
+    public void showWhenSesion(){
+        if (Sesion.getInstance().isActiva()){
+            Button b = (Button) findViewById(R.id.btnGenero);
+            show(b);
+            b= (Button) findViewById(R.id.btnEliminarGenero);
+            show(b);
+            b = (Button) findViewById(R.id.btnAgregarPrograma);
+            show(b);
+        }
+    }
+
+    private void hide(View v){
+        v.setEnabled(false);
+        ((Button)v).setTextColor(Color.GRAY);
+    }
+
+    private void show(View v){
+        v.setEnabled(true);
+        ((Button)v).setTextColor(getResources().getColor(R.color.colorButtonText));
     }
 
     public void onClickConsultar(View v){

8939aa022289e1eb49a3ab218ca873de250e5b72
Camilo <camilonar09@gmail.com>
2016-04-11 23:47:27 -0500

    Se cambiaron unos textos para ser más acordes en el AgregarCanal

--- app/src/main/java/epiphany_soft/wtw/Activities/MainActivity.java
+++ app/src/main/java/epiphany_soft/wtw/Activities/MainActivity.java
@@ -46,6 +46,8 @@ public class MainActivity extends AppCompatActivity{
             hide(b);
             b = (Button) findViewById(R.id.btnAgregarPrograma);
             hide(b);
+            b = (Button) findViewById(R.id.btnAgregarCanal);
+            hide(b);
         }
     }
 
@@ -56,6 +58,8 @@ public class MainActivity extends AppCompatActivity{
             b= (Button) findViewById(R.id.btnEliminarGenero);
             show(b);
             b = (Button) findViewById(R.id.btnAgregarPrograma);
+            show(b);
+            b = (Button) findViewById(R.id.btnAgregarCanal);
             show(b);
         }
     }
